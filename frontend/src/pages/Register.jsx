21784a6e904eee776bb6efe5c7399783fe58fee4
Khadijatariq434 <khadijatariq434@gmail.com>
2025-04-27 15:11:29 +0530

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -26,7 +26,7 @@ const Register = () => {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:5000/api/user/register", {
+      const res = await fetch("https://shopix-backend-yzwb.onrender.com/api/user/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, password }),
