d8dff647d5a8b6715fc44dc6fc39b2f50c39a0b8
Khadijatariq434 <khadijatariq434@gmail.com>
2025-04-27 15:10:12 +0530

    Update ContactUs.jsx

--- frontend/src/pages/ContactUs.jsx
+++ frontend/src/pages/ContactUs.jsx
@@ -23,7 +23,7 @@ const ContactUs = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     
-    const response = await fetch("http://localhost:5000/api/contact", {
+    const response = await fetch("https://shopix-backend-yzwb.onrender.com/api/contact", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
