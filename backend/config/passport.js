c4cdcc2ff6d7721e6c260ce09b3d6b9239f7e3b0
Khadijatariq434 <khadijatariq434@gmail.com>
2025-05-08 13:38:57 +0530

    Update passport.js

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -5,7 +5,7 @@ import jwt from "jsonwebtoken";
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5000/auth/google/callback",
+    callbackURL: "https://shopix-backend-yzwb.onrender.com/auth/google/callback",
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
